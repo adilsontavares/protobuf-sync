e8a1335ba0b537a85ee54d5caf5bf195dbfb4e08
Adilson Tavares <adilsonmktavares@gmail.com>
2017-09-26 21:52:22 -0300

    Organização das mensagens

---
+++ src/Bridge/FromMongo.php
@@ -0,0 +1,48 @@
+<?php
+require __DIR__ . '/../Messages/Book.php';
+require __DIR__ . '/../Messages/CatalogItem.php';
+
+function many($items, $func) {
+
+    $result = array();
+
+    foreach ($items as $item) {
+        array_push($result, $func($item));
+    }
+
+    return $result;
+}
+
+function assign($obj, $mongo, $prop) {
+
+    $setProp = "set" . ucfirst($prop);
+
+    if ($prop === 'id')
+        $prop = '_id';
+
+    // printf("%s[%s] = mongo[%s]\n", gettype($obj), $setProp, $prop);
+
+    $obj->{$setProp}($mongo[$prop]);
+}
+
+function to_book($data) {
+
+    $book = new Messages\Book();
+    assign($book, $data, 'name');
+    assign($book, $data, 'price');
+
+    return $book;
+}
+
+function to_catalog($data) {
+
+    $item = new Messages\CatalogItem();
+    
+    assign($item, $data, 'id');
+    assign($item, $data, 'count');
+
+    $item->setBook(to_book($data['book']));
+
+    return $item;
+}
+?>
